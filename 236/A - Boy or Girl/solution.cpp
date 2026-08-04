e312d04e9961572024c0378384d2c403fdcb5e5c
Manish chauhan <nitm.manishchauhan@gmail.com>
2026-08-04 19:47:14 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    string s;
+    cin>>s;
+    set <char> st ;
+    for (int i = 0 ; i< s.size() ; i++){
+        st.insert(s[i]);
+    }
+    if ((st.size()%2) == 0){
+        cout <<"CHAT WITH HER!";
+    }
+    else{
+        cout <<"IGNORE HIM!";
+    }
+    return 0;
+}

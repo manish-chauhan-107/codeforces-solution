b856d70fc19d23344db221315db4490416e0dd7f
Manish chauhan <nitm.manishchauhan@gmail.com>
2026-08-05 15:53:16 +0530

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin>>s;
+    s[0] = toupper(s[0]);
+    for ( int i = 0 ; i < s.size(); i++){
+        cout<<s[i];
+    }
+    return 0;
+}

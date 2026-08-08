ed7fd2c87b05d824bd999fdd3973ccc064d31d0d
Manish chauhan <nitm.manishchauhan@gmail.com>
2026-08-09 01:48:33 +0530

    Add Lucky Division [A] from Codeforces

--- 122/A - Lucky Division/solution.cpp
+++ 122/A - Lucky Division/solution.cpp
@@ -2,15 +2,24 @@
 using namespace std;
  
 int main() {
- int x ;
- int h = 0;
- cin>>x;
- vector<int> v;
- if (x % 4 == 0 || x % 7 == 0 || x % 47 == 0||  x % 447 == 0|| x % 774 == 0 || x % 744 == 0 || x % 477 == 0){
-   cout<<"YES";
- }
- else{
-   cout<<"NO";
- }
- return 0;
+    int x;
+    cin >> x;
+ 
+    int lucky[] = {
+        4, 7,
+        44, 47, 74, 77,
+        444, 447, 474, 477,
+        744, 747, 774, 777
+    };
+ 
+    for (int i = 0; i < 14; i++) {
+        if (x % lucky[i] == 0) {
+            cout << "YES";
+            return 0;
+        }
+    }
+ 
+    cout << "NO";
+ 
+    return 0;
 }

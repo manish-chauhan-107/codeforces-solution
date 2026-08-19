78fa77a5d37871945ab3b9a8f9326c4e5dc2ee08
Manish chauhan <nitm.manishchauhan@gmail.com>
2026-08-20 00:01:43 +0530

    Add Arrival of the General [A] from Codeforces

--- 144/A - Arrival of the General/solution.cpp
+++ 144/A - Arrival of the General/solution.cpp
@@ -1,38 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+ 
 int main() {
-   int t, m , n ;
-   int p,q;
-   p = 0 ;
-   q = 0 ;
-   cin>>t;
-   vector<int> v(t,0);
-   for (int i = 0 ; i < t ; i++){
-    cin>>v[i];
+   int t; cin>>t;
+   int M = INT_MIN;
+   int m = INT_MAX;
+   int P = 0;
+   int p = 0;
+   int temp;
+   for(int i = 1; i<=t; i++){
+    cin>>temp;
+    if(temp>M){
+        M=temp;P=i;
+    }
+    if(temp<=m){
+        m=temp;p=i;
+    }
    }
-   m = *max_element(v.begin(), v.end());
-   n = *min_element(v.begin(), v.end());
-   bool flag = true;
-   for (int i = 0 ; i < t ; i++){
-        if (v[i] == m && flag){
-          if(i == 0){
-          p = 0;
-          flag = false;
-          }
-          else{
-            p = i;
-            flag = false;
-          }
-        }
-        else if(v[i] == n){
-          q = i;
-        }
-   }
-   if(p > q){
-   cout<<((p-1)+(t-q)-1);
-   }
-   else{
-    cout<<((p-1)+(t-q));
-   }
-   return 0;
-  }
+   int ans = P-1 + t-p;
+   if(P>p)ans--;
+   cout<<ans<<endl;
+    return 0;
+}
